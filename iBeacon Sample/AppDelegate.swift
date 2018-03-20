dd0e8dae39e2da1a64104c7a002415e4e3edf5d9
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-03-20 10:33:53 +0900

    Beacon ranging in background & push notifications

--- iBeacon Sample/AppDelegate.swift
+++ iBeacon Sample/AppDelegate.swift
@@ -7,34 +7,41 @@
 //
 
 import UIKit
+import UserNotifications
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: MainVC())
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            
+        }
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        
+        print("-----Now in Inactive-----")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        print("-----Now in Background-----")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-
+        print("-----Now in Foreground-----")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+        print("-----Now in Active-----")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        print("-----Now in Terminate-----")
     }
 
 
--- iBeacon Sample/ViewController/MainVC.swift
+++ iBeacon Sample/ViewController/MainVC.swift
@@ -8,53 +8,107 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
-class MainVC: UIViewController, CLLocationManagerDelegate{
+class MainVC: UIViewController, CLLocationManagerDelegate, UNUserNotificationCenterDelegate{
 
-    @IBOutlet weak var mainLabel: UILabel!
+    var count:Int = 0
     let locationManager = CLLocationManager()
+    @IBOutlet weak var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        UNUserNotificationCenter.current().delegate = self
+        setupLocationManager()
+    }
+    
+    //MARK:- LocationManager Setting
+    fileprivate func setupLocationManager(){
         locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.allowsBackgroundLocationUpdates = true
     }
     
     func rangeBeacons(){
-        guard let uuid = UUID(uuidString: "FDA50693-A4E2-4FB1-AFCF-C6EB07647825") else {return}
+        guard let uuid = UUID(uuidString: "74278BDA-B644-4520-8F0C-720EAF059935") else {return}
         let major:CLBeaconMajorValue = UInt16(10001)
         let minor:CLBeaconMinorValue = UInt16(19641)
         let identifier = "com.myhouse"
         let region = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: identifier)
+        region.notifyEntryStateOnDisplay = true
+        region.notifyOnExit = true
+        region.notifyOnEntry = true
         locationManager.startRangingBeacons(in: region)
+        locationManager.startMonitoring(for: region)
+    }
+
+}
+//MARK:- User Notification
+extension MainVC {
+    func localNotification(title: String, body: String){
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey: title, arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: body, arguments: nil)
+        content.sound = UNNotificationSound.default()
+        let request = UNNotificationRequest(identifier: "iBeacon", content: content, trigger: nil)
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: nil)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
     }
 }
-// MARK:- Location Manager Delegate
+//MARK:- Location Manager Delegate
 extension MainVC {
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        if state == .inside {
+            localNotification(title: "Inside", body: "Your in iBeacon's Region")
+            print("inside")
+        }else if state == .outside {
+            localNotification(title: "Outside", body: "Bye Bye")
+            print("outside")
+        }else if state == .unknown {
+            print("unknown")
+        }
+    }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
             rangeBeacons()
         }
     }
-    
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         guard let discoverBeacon = beacons.first?.proximity else {print("Couldn't find the beacon!"); return}
+        count += 1
         let backgroundColor: UIColor = {
             switch discoverBeacon {
             case .immediate:
+                print("immediate")
+                if count % 7 == 0 {
+                    localNotification(title: "Immediate", body: "Close!")
+                }
                 self.mainLabel.text = "immediate"
                 return UIColor.green
             case .near:
+                print("near")
+                if count % 7 == 0 {
+                    localNotification(title: "Near", body: "Almost close!")
+                }
                 self.mainLabel.text = "near"
                 return UIColor.orange
             case .far:
+                print("far")
+                if count % 7 == 0 {
+                    localNotification(title: "Far", body: "Too far!")
+                }
                 self.mainLabel.text = "far"
                 return UIColor.red
             case .unknown:
+                print("unknown")
                 self.mainLabel.text = "unknown"
                 return UIColor.black
             }
         }()
-        
         view.backgroundColor = backgroundColor
     }
 }
+
